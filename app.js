9bfb01a5223635a7d9d41e1a6ab598243165ecb9
juseokha-collab <sk_ccav@mnservice.co.kr>
2026-08-16 15:49:20 +0900

    쫌많이 이미지 업데이트 반영(위치 초기화), 알잘딱/레전드 박스에 꽉 차게 확대, 그라데이션 강화, 텍스트 테두리 약화

--- app.js
+++ app.js
@@ -2301,9 +2301,9 @@ const MEAL_AMOUNT_POINTS={'쫌쫌따리':20,'알잘딱':10,'쫌많이':-10,'레�
 const MEAL_AMOUNT_IMAGES={'쫌쫌따리':'amounts/jjomjjomttari.png','알잘딱':'amounts/aljaldak.png','쫌많이':'amounts/jjommanhi.png','레전드':'amounts/legend.png'};
 const MEAL_AMOUNT_IMAGE_STYLE={
   '쫌쫌따리':{x:50, y:15, scale:1.0},
-  '알잘딱':{x:50, y:15, scale:0.80},
-  '쫌많이':{x:50, y:60, scale:0.55},
-  '레전드':{x:60, y:15, scale:0.85}
+  '알잘딱':{x:50, y:15, scale:1.0},
+  '쫌많이':{x:50, y:15, scale:0.9},
+  '레전드':{x:60, y:15, scale:1.0}
 };
 const MEAL_FAST_POINTS=10;
 function mealScoreForEntries(entries){
